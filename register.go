f2bccbbd619b7603ad85614eb9fb4fbc7a208f04
Musyl Nikita <nikita.musyl@hofftech.ru>
2025-08-08 00:58:16 +0300

    update import path in registration file to reflect new package name

--- register.go
+++ register.go
@@ -2,7 +2,7 @@ package k6_counter
 
 import "go.k6.io/k6/js/modules"
 
-const importPath = "k6/x/example"
+const importPath = "k6/x/g-counter"
 
 func init() {
 	modules.Register(importPath, new(rootModule))
